6f3d4d8c7e5a43775a9820990cbada2e19f29730
Dedy ibrahim <dedyibrahym23@gmail.com>
2026-06-11 16:56:25 +0700

    fix: improve favicon discovery and canonical host

--- resources/views/components/favicon.blade.php
+++ resources/views/components/favicon.blade.php
@@ -1,6 +1,7 @@
-<link rel="icon" href="{{ asset('favicon.ico') }}?v=2" sizes="any">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=2">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=2">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=2">
-<link rel="manifest" href="{{ asset('site.webmanifest') }}?v=2">
+<link rel="icon" href="{{ asset('favicon.ico') }}?v=3" sizes="any">
+<link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon-192x192.png') }}?v=3">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=3">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=3">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=3">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}?v=3">
 <meta name="theme-color" content="#061A3A">
